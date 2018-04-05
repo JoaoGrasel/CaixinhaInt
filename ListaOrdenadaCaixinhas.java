89cbc65c852c642571030f9c988493dd65e98428
Joao <joao.gc1997@gmail.com>
2018-04-05 16:34:46 -0300

    Arquivo da Lista adicionado

---
+++ ListaOrdenadaCaixinhas.java
@@ -0,0 +1,42 @@
+public class ListaOrdenadaCaixinhas{
+	
+	CaixinhaInt atual;
+	CaixinhaInt primeira;
+	CaixinhaInt ultima;
+	int quantidade;
+	
+	public ListaOrdenadaCaixinhas(){
+		atual = null;
+		primeira = null;
+		ultima = null;
+		quantidade = 0;
+	}
+
+	public void inserirOrdenado(int elemento){
+		
+		if(quantidade = 0){
+	
+			CaixinhaInt caixinhaInserida = new CaixinhaInt(elemento, VOLTA NELA MESMA OU NULL?);
+			primeira = caixinhaInserida;		
+			ultima = caixinhaInserida;
+			quantidade = 1;		
+
+		} else {
+
+			CaixinhaInt caixinhaAtual = primeira;
+
+			while(elemento > caixinhaAtual.getDado){
+			
+				caixinhaAtual = caixinhaAtual.getProxima;
+
+		}	
+
+	}
+
+
+	public void 
+
+
+
+
+}
